a2b10066e68a9fc994696ec66692a334ffb6f3e2
NEROAT_73 <kikahi@naver.com>
2022-10-19 15:57:14 +0900

    FirstCommit

---
+++ src/main/java/com/lostarkarmy/LostarkarmyTestApplication.java
@@ -0,0 +1,13 @@
+package com.lostarkarmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkarmyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostarkarmyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lostarkarmy/LostarkarmyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostarkarmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkarmyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
